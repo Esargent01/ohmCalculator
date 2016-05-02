e28ed7fecb2efd874ad2e6fda0f63a88a5025abd
Earl Sargent <esargent01@gmail.com>
2016-05-01 21:57:34 -0500

    Testing files

---
+++ js/test.js
@@ -0,0 +1,18 @@
+describe(‘ohm Calculator ’, function() {
+
+beforeEach(module(app));
+
+var calcCtrl,
+scope;
+
+beforeEach(inject(function ($rootScope, $controller) {
+scope = $rootScope.$new();
+calcCtrl = $controller('calcCtrl', {
+$scope: scope
+});
+}));
+it('calculates the ohm resistance', function () {
+expect(scope.ohms).not.toBeUndefined();
+});
+
+});
